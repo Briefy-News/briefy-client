829b98b2139912bb655d92d7c3c95be3b504ee8e
Choi-hyeona <xhrl523@naver.com>
2024-02-04 01:46:13 +0900

    feat: SearchNewsLetterPage, SearchArticlePage제작

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
-import { Outlet, useMatch, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useMatch, useNavigate } from 'react-router-dom';
 import Footer from 'src/components/Footer/Footer';
 
 function NewsPage() {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const isCategoryPage = useMatch('/news/:category');
+  const isNewsPage = isCategoryPage || ['/news/search', '/news/search/newsletter', '/news/search/article'].includes(pathname);
   useEffect(() => {
-    if (!isCategoryPage) {
+    if (!isNewsPage) {
       navigate('/news/all', { replace: true });
     }
   }, []);
