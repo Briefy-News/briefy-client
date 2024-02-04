6bbbc41f748e706bdc836bf81df403c373eb5aca
Choi-hyeona <xhrl523@naver.com>
2024-02-04 18:36:16 +0900

    feat: MyArticlePage, MyNewsletterPage, SavedArticlePage 제작

--- src/components/Profile/MyNewsInfo.tsx
+++ src/components/Profile/MyNewsInfo.tsx
@@ -2,9 +2,9 @@ import { NavLink } from 'react-router-dom';
 
 function MyNewsInfo() {
   const newsInfo = [
-    { id: 1, title: '안 읽은 아티클', num: '00' },
-    { id: 2, title: '마이 뉴스레터', num: '00' },
-    { id: 3, title: '저장된 아티클', num: '00' },
+    { id: 1, title: '안 읽은 아티클', num: '00', path: 'article/unread' },
+    { id: 2, title: '마이 뉴스레터', num: '00', path: 'newsletter' },
+    { id: 3, title: '저장된 아티클', num: '00', path: 'saved' },
   ];
 
   return (
@@ -21,16 +21,18 @@ function MyNewsInfo() {
         </div>
 
         <ul className="flex">
-          {newsInfo.map(({ id, title, num }) => (
+          {newsInfo.map(({ id, title, num, path }) => (
             <li key={id} className="flex justify-between items-center">
-              <div className="w-full min-w769-max-w1080:w-[120px] lg:w-[132px]  flex flex-col justify-between">
-                <span className="text-black500 text-sm md:text-base">
-                  {title}
-                </span>
-                <span className="mt-[4px] md:mt-[8px] text-black900 text-base md:text-2xl font-semibold">
-                  {num}
-                </span>
-              </div>
+              <NavLink to={`/profile/${path}`}>
+                <div className="w-full min-w769-max-w1080:w-[120px] lg:w-[132px]  flex flex-col justify-between">
+                  <span className="text-black500 text-sm md:text-base">
+                    {title}
+                  </span>
+                  <span className="mt-[4px] md:mt-[8px] text-black900 text-base md:text-2xl font-semibold">
+                    {num}
+                  </span>
+                </div>
+              </NavLink>
               {id !== newsInfo.length && <hr className="mx-[10px] min-w769-max-w1080:mx-[20px] lg:mx-[60px] w-[1px] h-[40px] bg-black300" />}
             </li>
           ))}
--- src/pages/MyNewsletterPage.tsx
+++ src/pages/MyNewsletterPage.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
 import FadeInLayout from 'src/components/Layout/FadeInLayout';
+import CustomSelect from 'src/components/common/CustomSelect';
+import InfiniteNewsLetter from 'src/components/common/InfiniteNewsLetter';
 
 function MyNewsletterPage() {
+  const [orderBy, setOrderBy] = useState('latest');
+  const orderByList = [
+    { value: 'subscribers', label: '구독 많은순' },
+    { value: 'latest', label: '최신순' },
+    { value: 'oldest', label: '오래된순' },
+    { value: 'alphabet', label: '가나다순' },
+  ];
+  const customSelectProps = { orderBy, setOrderBy, list: orderByList };
+
   return (
-    <FadeInLayout>MyNewsletterPage</FadeInLayout>
+    <FadeInLayout>
+      <h2 className="flex items-center h-[44px] md:h-[64px] text-base md:text-2xl text-black900 font-semibold">
+        마이 뉴스레터
+      </h2>
+      <div className="flex justify-end">
+        <CustomSelect {...customSelectProps} />
+      </div>
+      <div className="pt-[16px] md:pt-[24px] pb-[36px] md:pb-[132px]">
+        <InfiniteNewsLetter />
+      </div>
+    </FadeInLayout>
   );
 }
 
