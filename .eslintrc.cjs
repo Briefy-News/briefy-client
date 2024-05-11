d8c109da4f2fa807b35f6504536e7d39c51b0a8d
Choi-hyeona <xhrl523@naver.com>
2024-05-11 21:42:21 +0900

    chore: png

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,11 @@
 module.exports = {
   root: true,
   extends: ['@tata-v/eslint-config-react'],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint/eslint-plugin'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    'jsx-a11y/control-has-associated-label': 'off',
-    'react/require-default-props': 'off',
-    'consistent-return': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
-    'import/order': 'off'
   },
-}
+};
